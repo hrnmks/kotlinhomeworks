622212db585bb06b7de07a4352530f73d7ae703f
hrnmks <148820386+hrnmks@users.noreply.github.com>
2025-04-27 17:19:32 +0300

    Add files via upload
    HW1

---
+++ Odev1.kt
@@ -0,0 +1,59 @@
+package com.example.kotlinderslerii.nesne_tabanli_programlama
+
+fun main() {
+    fun main() {
+        // Kategoriler 1
+        val sehir = "İstanbul"
+        val ulke = "Türkiye"
+        val telefon = "+905555555555"
+        val postaKodu = "34000"
+        val email = "ornek@example.com"
+        val meslek = "Yazılım Mühendisi"
+        val stokMiktari = 120
+        val musteriAdi = "Ahmet Yılmaz"
+        val bakiye = 2500.75
+        val dogumGunu = "1998-04-15"
+        val maas = 15000.0
+        val medeniDurum = "Bekar"
+        val urunYorum = "Ürün çok kaliteli!"
+
+        // Kategoriler 2
+        val odemeTarihi = "2025-05-01"
+        val odeme = 499.99
+        val siparisAdeti = 3
+        val arabaModeli = "Toyota Corolla"
+        val kitapAdi = "Suç ve Ceza"
+        val yayinlamaTarihi = "1866"
+        val indirimMiktari = 20 // yüzde olarak
+        val odaSayisi = 5
+        val enlem = 41.0082
+        val boylam = 28.9784
+        val urunAdi = "Bluetooth Kulaklık"
+        val yemekFiyati = 89.90
+
+        // Kategoriler 3
+        val marka = "Apple"
+        val muzikAdi = "Bohemian Rhapsody"
+        val videoSuresi = "2 saat 15 dakika"
+        val urunPuani = 4.7
+        val resimAdi = "manzara.jpg"
+        val dosyaFormati = "jpg"
+        val renk = "Mavi"
+        val renkKodu = "#0000FF"
+        val telefonModeli = "iPhone 15"
+        val ekranBoyutu = 6.1
+        val agirlik = 185.0 // gram
+
+        // Kategoriler 4
+        val ulusalGun = "29 Ekim Cumhuriyet Bayramı"
+        val tatilGunu = "01 Mayıs Emek ve Dayanışma Günü"
+        val rezervasyonTarihi = "2025-06-10"
+        val sokakAdi = "İstiklal Caddesi"
+        val otobusHatti = "34AS"
+        val kalanDakika = 15
+        val takipKodu = "TR123456789"
+        val kuponSuresi = "30 gün"
+        val kuponKodu = "INDIRIM30"
+        val faturaAdresi = "Şişli, İstanbul, Türkiye"
+    }
+}
